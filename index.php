a3cfcf33c140f142ee665be96c9d5cd87a9d5f3e
yeedomliu <36749952@qq.com>
2020-03-06 08:50:02 +0800

    automate_submit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+switch ($_SERVER['PATH_INFO']) {
+    case '/env' :
+        echo @$_REQUEST['version'];
+        break;
+    case '/fetch' :
+        echo file_get_contents(@$_REQUEST['url']);
+        break;
+    default:
+        echo '<pre>';
+        print_r($_SERVER);
+}
